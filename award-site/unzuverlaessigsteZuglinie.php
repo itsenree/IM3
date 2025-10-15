d25c4f393f6a9838fff3fdf7f00fe0db9c7f8e8d
itsenree <75910282+itsenree@users.noreply.github.com>
2025-10-15 13:43:49 +0200

    styles

---
+++ award-site/unzuverlaessigsteZuglinie.php
@@ -0,0 +1,61 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SBB Railbait Awards</title>
+    <link rel="stylesheet" href="../css/award.css">
+</head>
+<body>
+    <header>
+        <h1>SBB </br> Railbait Awards</h1>
+    </header>
+
+    <section>
+        <h2 class="award-title">Unzuverlässigste Zuglinien</h2>
+        <container class="detail_container">
+            <div class="category_selection">
+                <a class="detail_active" href="#">Aktuell</a>
+                <a class="detail_name" href="#">Rekorde</a>
+                <a class="detail_name" href="#">Preisträger</a>
+            </div>
+            <table>
+                <tr>
+                    <th>Rang</th>
+                    <th>Zuglinie</th>
+                    <th>Verspätungen</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>S11</td>
+                    <td>42</td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>ICE</td>
+                    <td>41</td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>ICE</td>
+                    <td>35</td>
+                </tr>
+                <tr>
+                    <td>4.</td>
+                    <td>ICE</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td>5.</td>
+                    <td>ICE</td>
+                    <td>28</td>
+                </tr>
+            </table>
+        </container>
+    </section>
+
+</html>
--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $verspaetetsteHaltestelleOverviev = "5";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SBB Railbait Awards</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <header>
