6e01f2bbb48086894188a27ca8d2f4f6320375e0
Maya Nikita <maya@MacBook-Pro-von-Maya.local>
2025-10-16 12:26:01 +0200

    endpoints for current awards

---
+++ php/unload/unzuverlässigste-zuglinie-aktuell.php
@@ -0,0 +1,20 @@
+<?php
+// Datenbankkonfiguration einbinden
+require_once '../config.php';
+
+// Header setzen, um JSON-Inhaltstyp zurückzugeben
+header('Content-Type: application/json');
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+    $sql = "SELECT zuglinie, COUNT(ID) FROM Verspaetungen WHERE ausfall = 0 GROUP BY zuglinie ORDER BY COUNT(ID) DESC";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $results = $stmt->fetchAll();
+
+    // Ausgabe der Ergebnisse
+    echo json_encode($results);
+} catch (PDOException $e) {
+    // Gibt eine Fehlermeldung zurück, wenn etwas schiefgeht
+    echo json_encode(['error' => $e->getMessage()]);
+}
